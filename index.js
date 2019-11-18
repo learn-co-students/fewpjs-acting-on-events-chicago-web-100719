d07c477a1f483257dc7294fdd8b006347a6ae156
github NikolaCzv <github nikola.raicic@hotmail.com>
2019-11-18 15:39:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+dodger.style.left;
+
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
